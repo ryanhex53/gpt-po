23bd896db6e2fceafd33fdcba82e8a08d801de9c
Glenn Schmidt <glenn.schmidt@skills-base.com>
2025-01-28 11:52:52 +1100

    Use -S argument with /usr/bin/env
    To support passing parameters to ‘node’

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env node --no-warnings=ExperimentalWarning
+#!/usr/bin/env -S node --no-warnings=ExperimentalWarning
 
 import { Command, Option } from "commander";
 import path from "path";
